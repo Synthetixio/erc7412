b2d7feb515349ec019cc3b50d52902300955bd10
Daniel Beal <git@dbeal.dev>
2023-09-08 14:33:42 -0700

    add e2e test with pyth

--- src/adapters/pyth.ts
+++ src/adapters/pyth.ts
@@ -3,37 +3,43 @@ import { Adapter } from "../adapter";
 import * as viem from "viem";
 
 export class PythAdapter implements Adapter {
-  private connection: EvmPriceServiceConnection;
-  constructor(endpoint: string) {
-    this.connection = new EvmPriceServiceConnection(endpoint);
-  }
+	private connection: EvmPriceServiceConnection;
+	constructor(endpoint: string) {
+		this.connection = new EvmPriceServiceConnection(endpoint);
+	}
 
-  getOracleId(): string {
-    return "PYTH";
-  }
+	getOracleId(): string {
+		return "PYTH";
+	}
 
-  async fetchOffchainData(
-    _client: viem.Client,
-    _requester: viem.Address,
-    data: viem.Hex
-  ): Promise<viem.Hex> {
-    const [priceIds, updateType, stalenessTolerance] = viem.decodeAbiParameters([
-      { name: "updateType", type: "uint8" },
-      { name: "stalenessTolerance", type: "uint64" },
-      { name: "priceIds", type: "bytes32[]" },
-    ], data);
+	async fetchOffchainData(
+		_client: viem.Client,
+		_requester: viem.Address,
+		data: viem.Hex,
+	): Promise<viem.Hex> {
+		const [updateType, stalenessTolerance, priceIds] = viem.decodeAbiParameters(
+			[
+				{ name: "updateType", type: "uint8" },
+				{ name: "stalenessTolerance", type: "uint64" },
+				{ name: "priceIds", type: "bytes32[]" },
+			],
+			data,
+		);
 
-    if (updateType as number !== 1) {
-      throw new Error(`update type ${updateType} not supported`);
-    }
+		if ((updateType as number) !== 1) {
+			throw new Error(`update type ${updateType} not supported`);
+		}
 
-    let updateData = await this.connection.getPriceFeedsUpdateData(priceIds as string[]);
+		let updateData = await this.connection.getPriceFeedsUpdateData(priceIds as string[]);
 
-    return viem.encodeAbiParameters([
-      { type: "uint8", name: "updateType" },
-      { type: "uint64", name: "stalenessTolerance" },
-      { type: "bytes32[]", name: "priceIds" },
-      { type: "bytes[]", name: "updateData" },
-    ], [updateType, stalenessTolerance, priceIds, updateData]);
-  }
+		return viem.encodeAbiParameters(
+			[
+				{ type: "uint8", name: "updateType" },
+				{ type: "uint64", name: "stalenessTolerance" },
+				{ type: "bytes32[]", name: "priceIds" },
+				{ type: "bytes[]", name: "updateData" },
+			],
+			[updateType, stalenessTolerance, priceIds, updateData],
+		);
+	}
 }
--- src/parseError.ts
+++ src/parseError.ts
@@ -2,6 +2,11 @@ import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
 export function parseError(error: viem.CallExecutionError): viem.Hex {
-  return ((error as viem.CallExecutionError).cause as any).cause.error
-    .data as viem.Hex;
+	try {
+		return ((error as viem.CallExecutionError).cause as any).cause.error.data as viem.Hex;
+	} catch (err) {
+		// rethrow the error (and log it so we can see the original)
+		console.error("got unknown error in erc7412 parse", error);
+		throw error;
+	}
 }
