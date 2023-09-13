2d99763feaca67c13ea4d3e1bfa93574f18e3d6d
Daniel Beal <git@dbeal.dev>
2023-09-13 12:34:49 -0700

    progress.

--- src/parseError.ts
+++ src/parseError.ts
@@ -2,12 +2,6 @@ import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
 export function parseError(error: any): viem.Hex {
-	console.debug("decoding error output", error);
-	console.debug("cause 1", error.cause);
-	console.debug("OK", JSON.stringify(error.cause));
-	console.debug("cause 2", error.cause.cause);
-	console.debug("cause 3", error.cause.cause.cause);
-	console.debug("cause 3 error", error.cause.cause.cause.error);
 	try {
 		if (error.cause?.cause?.error?.data) {
 			return error.cause?.cause?.error?.data;
