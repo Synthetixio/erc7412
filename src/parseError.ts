99f1d7f958f8af5c2d6c29a2e2fd70a4bba02c1e
Daniel Beal <git@dbeal.dev>
2023-09-13 11:47:29 -0700

    add a metric ton of extra log args

--- src/parseError.ts
+++ src/parseError.ts
@@ -2,7 +2,13 @@ import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
 export function parseError(error: any): viem.Hex {
-	console.debug("decoding error output", error, error.data);
+	console.debug(
+		"decoding error output",
+		error,
+		error.error,
+		JSON.stringify(error),
+		JSON.stringify(error.error),
+	);
 	try {
 		if (error.cause?.cause?.error?.data) {
 			return error.cause?.cause?.error?.data;
