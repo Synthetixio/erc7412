d73ee7b39c548e31d768b385a922d042da76524b
Daniel Beal <git@dbeal.dev>
2023-09-13 10:18:13 -0700

    fix error handling

--- src/parseError.ts
+++ src/parseError.ts
@@ -1,17 +1,16 @@
 import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
-export function parseError(error: viem.CallExecutionError): viem.Hex {
-  try {
-    if (((error as viem.CallExecutionError).cause as any).cause.error.data) {
-      return ((error as viem.CallExecutionError).cause as any).cause.error
-        .data as viem.Hex;
-    }
+export function parseError(error: any): viem.Hex {
+	try {
+		if (error.cause?.cause?.error?.data) {
+			return error.cause?.cause?.error?.data;
+		}
 
-    return (error as any).data as viem.Hex;
-  } catch (err) {
-    // rethrow the error (and log it so we can see the original)
-    console.error("got unknown error in erc7412 parse", error);
-    throw error;
-  }
+		return error.data as viem.Hex;
+	} catch (err) {
+		// rethrow the error (and log it so we can see the original)
+		console.error("got unknown error in erc7412 parse", error);
+		throw error;
+	}
 }
