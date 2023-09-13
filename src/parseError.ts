0fd47f7b432d112bdb1a6d27fbee353d89cb946f
Daniel Beal <git@dbeal.dev>
2023-09-13 12:01:59 -0700

    remove a cause

--- src/parseError.ts
+++ src/parseError.ts
@@ -2,7 +2,7 @@ import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
 export function parseError(error: any): viem.Hex {
-	console.debug("decoding error output", error, error.error, JSON.stringify(error.error));
+	console.debug("decoding error output", error, error.cause);
 	console.log("FOR CLARITY", JSON.stringify(error.error));
 	console.log("SO THE ERROR IS", error.error.data);
 	try {
@@ -10,8 +10,8 @@ export function parseError(error: any): viem.Hex {
 			return error.cause?.cause?.error?.data;
 		}
 
-		if (error.error.data) {
-			return error.error.data;
+		if (error.cause?.error?.data) {
+			return error.cause?.error?.data;
 		}
 	} catch (err) {}
 	// rethrow the error (and log it so we can see the original)
