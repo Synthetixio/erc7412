a8858cad9f9174b226335f905deb28bb0270dd48
0xDmitry <sign-69@mail.ru>
2023-10-06 18:57:14 +0400

    Fix parseError

--- src/parseError.ts
+++ src/parseError.ts
@@ -2,18 +2,20 @@ import * as viem from "viem";
 
 // TODO: Generalize this. See https://github.com/usecannon/cannon/blob/main/packages/builder/src/error/index.ts
 export function parseError(error: any): viem.Hex {
-	try {
-		if (error.cause?.cause?.error?.data) {
-			return error.cause?.cause?.error?.data;
-		}
-
-		if (error.cause?.cause?.cause?.error?.data) {
-			return error.cause?.cause?.cause?.error?.data;
-		}
-	} catch (err) {
-		console.error("exception in erc7412 error parser:", err);
-	}
-	// rethrow the error (and log it so we can see the original)
-	console.error("got unknown error in erc7412 parse", error);
-	throw error;
+  try {
+    if (error.cause?.data) {
+      return error.cause?.data;
+    }
+    if (error.cause?.cause?.error?.data) {
+      return error.cause?.cause?.error?.data;
+    }
+    if (error.cause?.cause?.cause?.error?.data) {
+      return error.cause?.cause?.cause?.error?.data;
+    }
+  } catch (err) {
+    console.error("exception in erc7412 error parser:", err);
+  }
+  // rethrow the error (and log it so we can see the original)
+  console.error("got unknown error in erc7412 parse", error);
+  throw error;
 }
